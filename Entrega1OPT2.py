b89457cbc806a1f4807635c11f71fbab4b85d9d2
nuriaaaoc <144157555+nuriaaaoc@users.noreply.github.com>
2024-03-16 15:39:32 +0100

    Update Entrega1OPT2.py

--- Entrega1OPT2.py
+++ Entrega1OPT2.py
@@ -1,4 +1,21 @@
 import gurobipy as gp
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from pulp import *
+
+# Cargar el conjunto de datos
+data = pd.read_csv('BostonHousing.csv')
+
+# Mostrar las primeras filas del conjunto de datos
+print(data.head())
+
+# Estadísticas descriptivas del conjunto de datos
+print(data.describe())
+
+# Correlación entre las variables
+print(data.corr())
 
 
 
