348f46ee4879d167d57b198e91fe4e87d83e6ad8
daviddddd11 <163660366+daviddddd11@users.noreply.github.com>
2024-03-16 16:12:13 +0100

    Update Entrega1OPT2.py

--- Entrega1OPT2.py
+++ Entrega1OPT2.py
@@ -18,7 +18,32 @@ print(data.describe())
 print(data.corr())
 
 
+# Histograma para la columna 'crim'
+plt.figure(figsize=(10, 6))
+plt.hist(data['crim'], bins=30, color='blue', edgecolor='black')
+plt.title('Histograma de la Tasa de Criminalidad (crim)')
+plt.xlabel('Tasa de Criminalidad')
+plt.ylabel('Frecuencia')
+plt.grid(axis='y', alpha=0.75)
 
+plt.show()
 
+# Boxplot para la columna 'zn'
+plt.figure(figsize=(8, 6))
+plt.boxplot(data['zn'], vert=False, patch_artist=True)
+plt.title('Boxplot de la Proporción de Suelo Residencial Urbanizado')
+plt.xlabel('Proporción de suelo residencial urbanizado en lotes de más de 25,000 pies cuadrados (%)')
+plt.grid(axis='x', alpha=0.75)
 
+plt.show()
+
+# Histograma para la columna 'indus'
+plt.figure(figsize=(10, 6))
+plt.hist(data['indus'], bins=30, color='green', edgecolor='black')
+plt.title('Histograma de la Proporción de Acres Comerciales No Minoristas por Ciudad')
+plt.xlabel('Proporción de Acres Comerciales No Minoristas por Ciudad')
+plt.ylabel('Frecuencia')
+plt.grid(axis='y', alpha=0.75)
+
+plt.show()
 
